3da1535b1654b738a52d7d8ee45dd3eece549869
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2025-04-23 09:22:41 +0000

    feat(dev) cree le query qui cree le table devlopeur

--- app/Models/Devlopeur.js
+++ app/Models/Devlopeur.js
@@ -1,12 +1,30 @@
+const db = require('../demo_db_connection.js');
 
-class Devlopeur{
-    constructord(name,email,password){
+class Devlopeur {
+
+    constructor(){
         this.name;
-        this.email;
         this.password;
     }
-     Register(request) {
-        
+
+  static async CreateTable(){
+
+ let   query = `CREATE TABLE IF NOT EXISTS devlopeurs (
+        id  SERIAL PRIMARY KEY ,
+        name varchar(255) unique,
+        password varchar(255),
+        created_at TIMESTAMP DEFAULT NOW()
+    )`;
+
+  await db.query(query);
+
+    }
+
+    
+
+   async createDev(){
+       let  query = `INSERT INTO devlopeurs (name,password) VALUES ($name,$password) returning *;`;
+        const result = await db.query(query, [this.name, this.email]);
         
     }
 }
--- demo_db_connection.js
+++ demo_db_connection.js
@@ -4,23 +4,16 @@ const client = new Client({
   host: "localhost",
   user: "postgres",
   password: "root",
+  database: "DevBook",
   port: 5432
 });
 
 client.connect((err) => {
   if (err) {
-    console.log('Erreur de connexion:', err);
+    console.error('Erreur de connexion à DevBook:', err.stack);
   } else {
-    console.log('Connecté avec succès');
-
-    client.query('CREATE DATABASE "DevBook"', (err) => {
-      if (err) {
-        console.log('Erreur lors de la création:', err.message);
-      } else {
-        console.log('Base de données créée avec succès');
-      }
-
-      client.end(); 
-    });
+    console.log('Connecté à la base DevBook');
   }
 });
+
+module.exports = client;
